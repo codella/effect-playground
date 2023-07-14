c8cbb122fb23567642c2b8586ec6bc1e6e140e09
Mauro Codella <mauro.codella@gmail.com>
2023-07-14 22:03:30 +0200

    Add flattening with Effect.all

---
+++ src/effect-flattening.ts
@@ -0,0 +1,22 @@
+import { Effect } from "effect";
+
+declare const firstEffect: Effect.Effect<R1, E1, A1>;
+declare const secondEffect: Effect.Effect<R2, E2, A2>;
+
+// @ts-ignore
+const resultingEffect: Effect.Effect<R1 | R2, E1 | E2, [A1, A2]> = Effect.all([
+  firstEffect,
+  secondEffect,
+]);
+
+/*
+ * Defining sutypes used above
+ */
+
+type R1 = { _requirement: unknown };
+type E1 = { _tag: unknown };
+type A1 = { _value: unknown };
+
+type R2 = { _requirement: unknown };
+type E2 = { _tag: unknown };
+type A2 = { _value: unknown };
