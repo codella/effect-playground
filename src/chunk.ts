390138190ecb1d2b0d2fdd86569e5efba1afaf41
Mauro Codella <mcodella@zendesk.com>
2023-07-19 13:43:11 +0200

    more

---
+++ src/chunk.ts
@@ -0,0 +1,11 @@
+import { Chunk } from "effect";
+
+/*
+ * A Chunk<A> represents a chunk of values of type A.
+ * Chunks are usually backed by arrays, but expose a purely functional,
+ * safe interface to the underlying elements, and they become lazy on
+ * operations that would be costly with arrays, such as repeated concatenation.
+ * Like lists and arrays, Chunk is an ordered collection.
+/*
+
+Chunk.of
