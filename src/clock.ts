0fbc441a73ac010fa60fff1024c38d6d5b00aa77
Mauro Codella <mcodella@zendesk.com>
2023-07-19 16:37:34 +0200

    clock

---
+++ src/clock.ts
@@ -0,0 +1,8 @@
+import { Effect as T, Clock } from "effect";
+
+const program = Clock.Clock.pipe(
+  T.flatMap((clock) => clock.currentTimeMillis),
+  T.flatMap((millis) => T.log(millis.toString())),
+  T.provideService(Clock.Clock, Clock.make()),
+  T.runPromise
+);
