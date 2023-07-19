f116efcc01498aa9ef691380ffc82d4907138f6a
Mauro Codella <mcodella@zendesk.com>
2023-07-19 16:38:24 +0200

    clock

--- src/clock.ts
+++ src/clock.ts
@@ -1,6 +1,6 @@
 import { Effect as T, Clock } from "effect";
 
-const program = Clock.Clock.pipe(
+Clock.Clock.pipe(
   T.flatMap((clock) => clock.currentTimeMillis),
   T.flatMap((millis) => T.log(millis.toString())),
   T.provideService(Clock.Clock, Clock.make()),
