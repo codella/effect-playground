ef001f443e2af26866d1259b669fbd8473a56d4c
Mauro Codella <mauro.codella@gmail.com>
2023-07-19 19:52:03 +0200

    chunk

--- src/chunk.ts
+++ src/chunk.ts
@@ -1,4 +1,4 @@
-import { Chunk } from "effect";
+import { Chunk, pipe } from 'effect';
 
 /*
  * A Chunk<A> represents a chunk of values of type A.
@@ -8,4 +8,18 @@ import { Chunk } from "effect";
  * Like lists and arrays, Chunk is an ordered collection.
  */
 
-Chunk.of;
+// Chunk.append
+const chunk1 = pipe(
+  Chunk.empty(),
+  Chunk.append('another')
+);
+printChunk(chunk1);
+
+// Chunk.
+// const chunk2 = Chunk.empty()
+
+/* utility functions */
+
+function printChunk(chunk: Chunk.Chunk<unknown>) {
+  console.log(Chunk.toReadonlyArray(chunk));
+}
