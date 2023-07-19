12d17ddf95a762551d29daf315a2bf1d13ea3883
Mauro Codella <mcodella@zendesk.com>
2023-07-19 16:37:59 +0200

    clock

--- src/clock.ts
+++ src/clock.ts
@@ -4,5 +4,6 @@ const program = Clock.Clock.pipe(
   T.flatMap((clock) => clock.currentTimeMillis),
   T.flatMap((millis) => T.log(millis.toString())),
   T.provideService(Clock.Clock, Clock.make()),
+  T.repeatN(5),
   T.runPromise
 );
