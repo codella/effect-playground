f93ed4b81895a6cf9b39e330b2e3d213d600cec1
Mauro Codella <mcodella@zendesk.com>
2023-07-20 10:35:40 +0200

    fix

--- src/clock.ts
+++ src/clock.ts
@@ -3,7 +3,7 @@ import { Effect, Clock } from 'effect';
 Clock.Clock.pipe(
   Effect.flatMap((clock) => clock.currentTimeMillis),
   Effect.flatMap((millis) => Effect.log(millis.toString())),
-  Effect.provideService(Clock.Clock, Clock.make()),
   Effect.repeatN(5),
+  Effect.provideService(Clock.Clock, Clock.make()),
   Effect.runPromise
 );
