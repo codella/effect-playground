71c9b471f8ab8e23136639239bddd6beee580d8e
Mauro Codella <mauro.codella@gmail.com>
2023-08-12 10:01:06 +0200

    bump effect

--- src/repeat.ts
+++ src/repeat.ts
@@ -1,29 +1,30 @@
-import { Effect, Schedule } from "effect";
+import { Effect, Schedule } from 'effect';
 
-await Effect.log("repeating 2 times with .repeatN").pipe(
+await Effect.log('repeating 2 times with .repeatN').pipe(
   Effect.repeatN(2),
   Effect.runSync /* not using a policy -- we can use runSync */
 );
 
 const policyOnce = Schedule.once;
-await Effect.log("repeating once").pipe(
+await Effect.log('repeating once').pipe(
   Effect.repeat(policyOnce),
   Effect.runPromise /* using a policy requires runPromise */
 );
 
 const policy2Times = Schedule.recurs(2);
-await Effect.log("repeating 2 times with .recurs").pipe(
+await Effect.log('repeating 2 times with .recurs').pipe(
   Effect.repeat(policy2Times),
   Effect.runPromise /* using a policy requires runPromise */
 );
 
-const policyRecurs5Times100MillisDelayBetween = Schedule.recurs(5).pipe(
-  Schedule.addDelay(() => "100 millis")
-);
+const policyRecurs5Times100MillisDelayBetween =
+  Schedule.recurs(5).pipe(
+    Schedule.addDelay(() => '100 millis')
+  );
 await Effect.log(
-  "delayed 300 millis, recurs 5 times with 100 millis delay between"
+  'delayed 300 millis, recurs 5 times with 100 millis delay between'
 ).pipe(
-  Effect.delay("300 millis"),
+  Effect.delay('300 millis'),
   Effect.repeat(
     policyRecurs5Times100MillisDelayBetween
   ) /* need to use "seconds" even on a singular second */,
